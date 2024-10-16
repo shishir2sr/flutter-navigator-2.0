b5591d60b66c2938a331b7522fa4ff578161ac4b
Yeasir Arefin Tusher <yeasir.arefin@bjitgroup.com>
2024-10-16 13:02:20 +0600

    Autoroutes

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,29 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:navigator_two_o/app_router.dart';
 
+import 'package:navigator_two_o/page_one.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  final _appRouter = AppRouter();
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerDelegate: AutoRouterDelegate(_appRouter),
-      routeInformationParser: _appRouter.defaultRouteParser(),
+    return MaterialApp(
+      title: 'Navigator 2.0',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const PageOne(),
+      onGenerateInitialRoutes: (initialRoute) => [
+        MaterialPageRoute(
+          settings: RouteSettings(name: initialRoute),
+          builder: (_) => const PageOne(),
+        ),
+      ],
     );
   }
 }
